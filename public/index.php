853ecd01575a3acb095c22b40fb63307453c32ce
DoINeedAName <jamesparish95@gmail.com>
2022-09-10 17:00:50 +1000

    Testing branch switching

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>LMS</title>
+  </head>
+  <body>
+    <header>
+      <h1>Github branching is easy</h1>
+    </header>
+    <b>I hope this works</b>
+    <footer>
+      <b>Everyone needs a footer</b>
+    </footer>
+  </body>
+</html>
